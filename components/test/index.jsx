1d932dfa63531d12d67c9272254f60785ae7cb93
Alexander Novikov <a.e.novikov.mk3@gmail.com>
2018-10-27 18:04:54 +0300

    feat: add feature9

--- components/test/index.jsx
+++ components/test/index.jsx
@@ -13,6 +13,7 @@ class TestComponent extends Component {
                 <div>feature 6</div>
                 <div>feature 7</div>
                 <div>feature 8</div>
+                <div>feature 9</div>
             </div>
         );
     }
